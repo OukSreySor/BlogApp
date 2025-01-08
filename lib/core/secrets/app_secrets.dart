1a521c16473ea5a5cb59e772357714659364019f
OukSreySor <ouk.sreysor11@gmail.com>
2025-01-08 20:33:40 +0700

    first commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://biynfddqutvikdisddhl.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpeW5mZGRxdXR2aWtkaXNkZGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNTk4OTcsImV4cCI6MjA1MTgzNTg5N30.Tx7i88vOmXPYTUA22XYwCk6FVqBbyWxjukUQHnhoYmo'; 
+}
---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,27 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+   Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl(this.supabaseClient);
+  @override
+  Future<String> loginWithEmailPassword({required String email, required String password, }) {
+    throw UnimplementedError();
+  }
+  @override
+  Future<String> signUpWithEmailPassword({required String name, required String email, required String password, }) {
+    throw UnimplementedError();
+  }
+
+}
